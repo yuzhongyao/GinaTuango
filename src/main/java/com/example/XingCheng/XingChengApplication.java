299886f401897a030a50e7cc69d62e2125f6cc0d
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2023-09-20 00:34:13 -0400

    spring boot initializer

---
+++ src/main/java/com/example/XingCheng/XingChengApplication.java
@@ -0,0 +1,13 @@
+package com.example.XingCheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XingChengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XingChengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/XingCheng/XingChengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.XingCheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XingChengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
