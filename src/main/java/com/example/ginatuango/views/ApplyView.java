e06fe0f390664c21cc2afdba8c70749ee54e6425
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2023-12-22 23:22:51 -0500

    add security configuration class
    set login page to created login view class and allow all to see application view

--- src/main/java/com/example/ginatuango/views/ApplyView.java
+++ src/main/java/com/example/ginatuango/views/ApplyView.java
@@ -4,9 +4,11 @@ import com.example.ginatuango.views.components.ApplyForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @PageTitle("Application")
 @Route(value = "/apply")
+@AnonymousAllowed
 public class ApplyView extends VerticalLayout {
 
     public ApplyView(){
