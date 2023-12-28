d912d78c9e93a1504eff6d7e34c65074837bbc6e
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2023-12-27 22:56:17 -0500

    Update LoginView.java
    use Vaadin login form instead

--- src/main/java/com/example/ginatuango/views/LoginView.java
+++ src/main/java/com/example/ginatuango/views/LoginView.java
@@ -2,8 +2,10 @@ package com.example.ginatuango.views;
 
 import com.example.ginatuango.data.entities.User;
 import com.example.ginatuango.views.components.LoginForm;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
@@ -13,6 +15,9 @@ import com.vaadin.flow.server.VaadinSession;
 @Route(value = "/login")
 public class LoginView extends VerticalLayout {
 
+    LoginI18n i18n = LoginI18n.createDefault();
+    private final com.vaadin.flow.component.login.LoginForm login = new com.vaadin.flow.component.login.LoginForm();
+
     public LoginView(){
         this.addClassName("login-form");
 
@@ -21,13 +26,25 @@ public class LoginView extends VerticalLayout {
 
 
 
-
         //background image div
         Div imageContainer = new Div();
         imageContainer.setWidthFull();
         imageContainer.addClassName("image-container");
 
-        add(title, new LoginForm());
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setForgotPassword("Apply");
+        
+
+        login.setI18n(i18n);
+        login.setAction("login");
+        login.addForgotPasswordListener(e ->{
+            UI ui = UI.getCurrent();
+            ui.navigate("/apply");
+        });
+
+
+
+        add(title, login);
 
 
 
