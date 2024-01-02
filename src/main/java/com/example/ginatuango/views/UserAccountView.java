752fc9c0bfec4768e06060fdc20ee7efd25c9797
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2024-01-01 21:50:07 -0500

    Create UserAccountView.java
    add blank user account details page

---
+++ src/main/java/com/example/ginatuango/views/UserAccountView.java
@@ -0,0 +1,26 @@
+package com.example.ginatuango.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@Route(value = "/user")
+@PageTitle("Account Details")
+@RolesAllowed("USER")
+public class UserAccountView extends VerticalLayout implements HasUrlParameter<Integer> {
+
+    public UserAccountView(){
+        H1 title = new H1("Welcome");
+        add(title);
+    }
+
+
+    @Override
+    public void setParameter(BeforeEvent beforeEvent, Integer integer) {
+
+    }
+}
