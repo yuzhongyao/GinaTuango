10e7fe02036f6a49c130f80aa3147255d37cd76e
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2024-01-14 23:50:53 -0500

    Update UserAccountView.java
    get user info and add name to welcome

--- src/main/java/com/example/ginatuango/views/user/UserAccountView.java
+++ src/main/java/com/example/ginatuango/views/user/UserAccountView.java
@@ -1,26 +1,51 @@
 package com.example.ginatuango.views.user;
 
+import com.example.ginatuango.data.entities.User;
+import com.example.ginatuango.services.UserService;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.security.AuthenticationContext;
 import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 @Route(value = "/user")
 @PageTitle("Account Details")
 @RolesAllowed("USER")
 public class UserAccountView extends VerticalLayout implements HasUrlParameter<Integer> {
 
-    public UserAccountView(){
-        H1 title = new H1("Welcome");
+    private int userId;
+    private final UserService userService;
+    private Optional<User> user;
+    private final transient AuthenticationContext authContext;
+
+    public UserAccountView(UserService userService, AuthenticationContext authenticationContext){
+        this.userService = userService;
+        this.authContext = authenticationContext;
+
+
+        user = userService.getUserByUsername(authContext.getAuthenticatedUser(UserDetails.class).get().getUsername());
+
+        String name;
+        if(user.isPresent()){
+            name = user.get().getName();
+        }
+        else{
+            name = "";
+        }
+
+        H1 title = new H1("Welcome " + name);
         add(title);
     }
 
 
     @Override
     public void setParameter(BeforeEvent beforeEvent, Integer integer) {
-
+        this.userId = integer;
     }
 }
