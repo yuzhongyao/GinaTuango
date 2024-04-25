d20e9b40a88837cd95fb15dc6064ba18dd507f15
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2024-04-25 02:21:19 -0400

    Update UserSpecificOrderView.java
    show user order items

--- src/main/java/com/example/ginatuango/views/user/UserSpecificOrderView.java
+++ src/main/java/com/example/ginatuango/views/user/UserSpecificOrderView.java
@@ -9,13 +9,18 @@ import com.example.ginatuango.views.AdminLayout;
 import com.example.ginatuango.views.MainView;
 import com.example.ginatuango.views.UserLayout;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.*;
 import com.vaadin.flow.spring.security.AuthenticationContext;
 import jakarta.annotation.security.RolesAllowed;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
 import java.util.Optional;
 
 @Route(value = "user/order",layout = UserLayout.class)
@@ -30,6 +35,9 @@ public class UserSpecificOrderView extends Div implements HasUrlParameter<Intege
     private final OrderService orderService;
 
     private Optional<User> user;
+    Grid<Object[]> orderGrid = new Grid<>();
+    H1 title = new H1();
+
 
 
 
@@ -41,11 +49,37 @@ public class UserSpecificOrderView extends Div implements HasUrlParameter<Intege
 
         user = userService.getUserByUsername(authenticationContext.getAuthenticatedUser(UserDetails.class).get().getUsername());
 
+        title.setText("Order ");
+        title.addClassName("center");
+        title.setWidthFull();
+
+
+        add(title, orderGrid);
+
+    }
+
+    private void configureGrid() {
+        List<Object[]> rows = orderService.getItemSalesByOrder(integer);
+        ListDataProvider<Object[]> dataProvider = DataProvider.ofCollection(rows);
+        orderGrid.setDataProvider(dataProvider);
+
+//        orderGrid.addColumn(arr -> arr[0]).setHeader("Customer");
+        orderGrid.addColumn(arr -> arr[1]).setHeader("Item");
+        orderGrid.addColumn(arr -> arr[2]).setHeader("Total");
+        orderGrid.addColumn(arr -> arr[3]).setHeader("Sale Type");
+        orderGrid.addColumn(arr -> arr[4]).setHeader("Date");
+        orderGrid.addColumn(arr -> arr[5]).setHeader("Cost");
+        orderGrid.addColumn(arr -> arr[6]).setHeader("On Sale");
+
+        orderGrid.
+
     }
 
     @Override
     public void setParameter(BeforeEvent beforeEvent, Integer integer) {
+
         this.integer = integer;
+        configureGrid();
     }
 
     @Override
